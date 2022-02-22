1d5fddee7a731ef5c7c31b28f1f3671247a0c82b
Mohammed Dawood <mahmedtony73@gmail.com>
2022-02-22 17:30:26 +0200

    Class Based component Life cycle methods
    1-componentDidMount
    2-componentDidUpdate
    3-componentWillUnmount

--- src/components/User.js
+++ src/components/User.js
@@ -5,6 +5,9 @@ class User extends Component {
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
   }
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
 }
 
 // const User = (props) => {
